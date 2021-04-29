5368e7eefad72ef95c78bae4ffaac2ad0a209860
MikaVainio <koti.mika.vainio@gmail.com>
2021-04-29 10:03:21 +0300

    Lisätty uusi näkymä

--- Henkiön_mittaukset_ja_ikä.sql
+++ Henkiön_mittaukset_ja_ikä.sql
@@ -1,3 +1,4 @@
-CREATE VIEW henkilo_mittaus_ika AS
+DROP VIEW "main"."henkilo_miitaus_ika";
+CREATE VIEW henkilo_mittaus_ika_v2 AS
 SELECT henkilo.henkilo_id, henkilo.sukunimi, henkilo.etunimi, mittaus.mittaus_id, mittaus.pituus, mittaus.paino, henkilo.spaiva, date('now') AS tanaan, (julianday('now') - julianday(henkilo.spaiva))/365 AS ika
-FROM henkilo LEFT OUTER JOIN mittaus ON henkilo.henkilo_id = mittaus.henkilo_id;
+FROM henkilo LEFT OUTER JOIN mittaus ON henkilo.henkilo_id = mittaus.henkilo_id
