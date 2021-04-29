3439e30f2ba87e79447e39cc8c380ba5c5efc587
MikaVainio <koti.mika.vainio@gmail.com>
2021-04-29 15:31:48 +0300

    Lisätty upper-metodeihin puuttuvat sulut

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ import kysymys # Tietojen syöttämiseen liittyvät kysymisrutiinit
 import luokat # Henkilö, Aikuinen ja Lapsi -luokkien määrittelyt
 
 # Varsinainen ohjema
-
 while True:
     
-    pass
-
     # Silmukka henkilötietojen kyselemiseen
-    lisaa_henkiloita = input('Lisätäänkö uusia henkilöitä? k/E')
+    lisaa_henkiloita = input('Lisätäänkö uusia henkilöitä? K/e')
+    if lisaa_henkiloita.upper() != 'E':
+        lisaa_henkiloita = 'K'
 
-
-    while lisaa_henkiloita.upper == 'K':
+    while lisaa_henkiloita.upper() == 'K':
         etunimi = input('etunimi: ')
         sukunimi = input('sukunimi: ')
         sukupuoli = kysymys.kysy_liukuluku('Sukupuoli nainen 0, mies 1: ', 0, 1)
@@ -28,7 +26,7 @@ while True:
 
     # Silmukka mittaustietojen kyselemiseen
     lisaa_mittauksia = input('Lisätäänkö uusia mittaustuloksia? k/E')
-    while lisaa_mittauksia.upper == 'K':
+    while lisaa_mittauksia.upper() == 'K':
         pituus = kysymys.kysy_liukuluku('Pituus (cm): ', 100, 250)
         paino = kysymys.kysy_liukuluku('Paino (kg): ', 30, 200)
     
